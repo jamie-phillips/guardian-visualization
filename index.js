544e26b87e26806f0f5b49868d0e0b45375822c6
Jamie Phillips <jmpjamie@gmail.com>
2022-07-01 15:34:34 +0100

    Daily api call for yesterday's articles

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const guardian = require("./guardian-search.js");
+const fs = require("fs");
+const mongodb = require("mongodb");
+const schedule = require("node-schedule");
+const { text } = require("express");
 
 const app = express();
 const port = 3000;
+const searchDataFP = "./searchData.json";
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Listening on port ${port}`);
-  guardian.updateDB(new Date("1800-01-01"));
+  await createIndex();
 });
 
 app.use(express.static("public"));
@@ -14,3 +19,70 @@ app.use(express.static("public"));
 app.get("/api", (req, res) => {
   res.json(req.query);
 });
+
+schedule.scheduleJob("0 1 * * *", async () => {
+  const searchData = getData(searchDataFP);
+  if (searchData != "") {
+    const date = searchData.date;
+    const success = await guardian.updateDB(new Date(date));
+    if (success) {
+      const today = new Date();
+      const data = {
+        date: `${today.getFullYear()}-${
+          today.getMonth() + 1
+        }-${today.getDate()}`,
+      };
+      saveData(data, searchDataFP);
+    } else {
+      console.log(`There was an error updating the database - ${date}`);
+    }
+  }
+});
+
+function saveData(data, filePath) {
+  var jsonData = JSON.stringify(data);
+  fs.writeFile(filePath, jsonData, function (err) {
+    if (err) {
+      console.log("There has been an error saving your configuration data.");
+      console.log(err.message);
+      return;
+    }
+    console.log("Data saved successfully.");
+  });
+}
+
+function getData(filePath) {
+  var jsonData = fs.readFileSync(filePath),
+    data;
+
+  try {
+    data = JSON.parse(jsonData);
+    console.log("Data read successfully.");
+    return data;
+  } catch (err) {
+    console.log("There has been an error parsing your JSON.");
+    console.log(err);
+    return "";
+  }
+}
+
+/*async function createIndex() {
+  const dbUrl = "mongodb://127.0.0.1:27017/";
+
+  var MongoClient = mongodb.MongoClient;
+  const client = new MongoClient(dbUrl);
+  const articlesCollection = client.db("test").collection("testData");
+  try {
+    await client.connect();
+    const result = await articlesCollection.createIndex({
+      webPublicationDate: 1,
+      webTitle: text,
+      bodyTextSummary: text,
+    });
+    console.log(`Index created successfully: ${result}`);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
+}*/
