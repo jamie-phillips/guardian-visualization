93b5e343f7b522c55b87131c84e3a9211314d486
Jamie Phillips <jmpjamie@gmail.com>
2022-07-01 15:35:09 +0100

    Daily api calls which adds yesterday's articles

--- guardian-search.js
+++ guardian-search.js
@@ -1,86 +1,133 @@
 const fetch = require("node-fetch");
+const sema = require("async-sema");
+const mongodb = require("mongodb");
 require("dotenv").config();
 
 const apiKey = process.env.API_KEY;
+var MongoClient = mongodb.MongoClient;
+const dbUrl = "mongodb://127.0.0.1:27017/";
 
 async function updateDB(lastSearchDate) {
-  let today = new Date();
-  let yesterday = new Date(today);
-  yesterday.setDate(yesterday.getDate() - 1);
+  const client = new MongoClient(dbUrl);
+  const tempCollection = client.db("articles").collection("tempData");
+  const articlesCollection = client.db("articles").collection("articleData");
 
-  let startYear = lastSearchDate.getFullYear();
-  let endYear = yesterday.getFullYear();
-  let startMonth;
-  let endMonth;
-  let startDay;
-  let endDay;
+  try {
+    // Set yesterday as yesterday's date at 00:00:00
+    let today = new Date();
+    let yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+    yesterday.setHours(0, 0, 0, 0);
 
-  let daysData = [];
-  let requestsQueue = [];
-  let allResponses = [];
+    // if today has already been searhed then return false
+    if (lastSearchDate > yesterday) {
+      throw "lastSearchDate is the same or after yesterday's date";
+    }
 
-  for (let year = startYear; year <= endYear; year++) {
-    startMonth = year == startYear ? lastSearchDate.getMonth() + 1 : 1;
-    endMonth = year == endYear ? yesterday.getMonth() + 1 : 12;
-    for (let month = startMonth; month <= endMonth; month++) {
-      startDay =
-        year == startYear && month == startMonth ? lastSearchDate.getDate() : 1;
-      endDay =
-        year == endYear && month == endMonth
-          ? yesterday.getDate()
-          : daysInMonth(month, year);
-      for (let day = startDay; day <= endDay; day++) {
-        let date = `${year}-${month}-${day}`;
-        let params = {
-          "from-date": date,
-          "to-date": date,
-          "page-size": 1,
-          "api-key": apiKey,
-        };
-        console.log(`${year}-${month}-${day}`);
-        let response = await guardianSearch(params);
-        let total = response.response.total;
-        let i = 1;
-        while (total > 0) {
-          let pageSize = total > 200 ? 200 : total;
-          requestsQueue.push({
+    await client.connect(); // Connect to MongoDB database
+    tempCollection.deleteMany({}); // Delete all documents in tempData collection
+
+    let requestsQueue = [];
+
+    let startYear = lastSearchDate.getFullYear();
+    let endYear = yesterday.getFullYear();
+    let startMonth;
+    let endMonth;
+    let startDay;
+    let endDay;
+
+    for (let year = startYear; year <= endYear; year++) {
+      startMonth = year == startYear ? lastSearchDate.getMonth() + 1 : 1;
+      endMonth = year == endYear ? yesterday.getMonth() + 1 : 12;
+      for (let month = startMonth; month <= endMonth; month++) {
+        startDay =
+          year == startYear && month == startMonth
+            ? lastSearchDate.getDate()
+            : 1;
+        endDay =
+          year == endYear && month == endMonth
+            ? yesterday.getDate()
+            : daysInMonth(month, year);
+        for (let day = startDay; day <= endDay; day++) {
+          let date = `${year}-${month}-${day}`;
+          let params = {
             "from-date": date,
             "to-date": date,
-            "page-size": pageSize,
-            page: i,
-            "order-by": "oldest",
+            "page-size": 1,
             "api-key": apiKey,
-          });
-          total -= pageSize;
-          i++;
+          };
+          console.log(date);
+          let response = await guardianSearch(params);
+          let total = response.response.total;
+          let i = 1;
+          while (total > 0) {
+            let pageSize = total > 200 ? 200 : total;
+            requestsQueue.push({
+              "from-date": date,
+              "to-date": date,
+              "page-size": pageSize,
+              page: i,
+              "order-by": "oldest",
+              "show-blocks": "body",
+              "api-key": apiKey,
+            });
+            total -= pageSize;
+            i++;
+          }
         }
       }
     }
-  }
 
-  let counter = 0;
-
-  for (let i = 0; i < requestsQueue.length; i += 12) {
-    let currentRequests = requestsQueue.slice(i, i + 12);
-    let responses = await fetchRequests(currentRequests);
-
-    for (let response of responses) {
-      console.log(response);
-      counter += response.response.pageSize;
+    let count = 0;
+    while (requestsQueue.length > 0) {
+      if (count > 5) {
+        throw "Requests failed more than 5 times";
+      }
+      requestsQueue = await fetchRequests(requestsQueue, tempCollection);
+      count++;
     }
-    //allResponses = allResponses.concat(responses);
+
+    await mergeCollections(articlesCollection, tempCollection);
+
+    client.close();
+    return true;
+  } catch (e) {
+    console.error(e);
+    await client.close();
+    return false;
   }
-  console.log(counter);
 }
 
-async function fetchRequests(requests) {
-  let promises = [];
-  for (let i = 0; i < requests.length; i++) {
-    promises[i] = guardianSearch(requests[i]);
+async function fetchRequests(requestsQueue, collection) {
+  noResponseQueue = [];
+  for (let i = 0; i < requestsQueue.length; i++) {
+    let currentRequest = requestsQueue[i];
+    let response = await guardianSearch(currentRequest);
+    console.log(response);
+    if (response.response.status != "ok") {
+      noResponseQueue.push(currentRequest);
+    } else {
+      let articles = response.response.results;
+      let formattedArticles = [];
+      for (let article of articles) {
+        let bodyTextSummary = "";
+        let bodies = article.blocks.body;
+        for (let body of bodies) {
+          bodyTextSummary += `${body.bodyTextSummary} `;
+        }
+        let formattedArticle = {
+          webTitle: article.webTitle,
+          webUrl: article.webUrl,
+          apiUrl: article.apiUrl,
+          webPublicationDate: article.webPublicationDate,
+          bodyTextSummary: bodyTextSummary,
+        };
+        formattedArticles.push(formattedArticle);
+      }
+      await inputData(collection, formattedArticles);
+    }
   }
-
-  let values = await Promise.all(promises);
-  return values;
+  return noResponseQueue;
 }
 
 async function guardianSearch(params) {
@@ -98,13 +145,21 @@ function daysInMonth(month, year) {
   return daysInMonth;
 }
 
+async function inputData(collection, obj) {
+  await collection.insertMany(obj);
+}
+
+async function mergeCollections(collection, tempCollection) {
+  await collection.insertMany(await tempCollection.find().toArray());
+}
+
 module.exports = { updateDB };
 
 /* 
 TODO:
- - use fetchRequests() for inital totals for each day
- - extract data for each article ready for database
- - input data into database
+ - use fetchRequests() for inital totals for each day - DONE
+ - extract data for each article ready for database - DONE
+ - input data into database - DONE
 
  - implement search database functionality
 */
